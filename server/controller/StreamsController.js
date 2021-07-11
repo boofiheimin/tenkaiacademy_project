831a5bd1b133167214f64801bdc0658e36664ada
Mheekwai Na Bangkok <boofiheimin@gmail.com>
2021-07-11 20:40:52 +0700

    add findbyid route

--- server/controller/StreamsController.js
+++ server/controller/StreamsController.js
@@ -1,23 +1,23 @@
 import Stream from "../models/Stream.js";
+import ErrorResponse from "../utils/ErrorResponse.js";
 
-export const getStreams = async (req, res) => {
-  const { query, offset, limit, sort = "-publishedAt" } = req.query;
+export const getStreams = async ({ query: reqQuery = {} }, res, next) => {
+  const { query = null, offset, limit, sort = "-publishedAt" } = reqQuery;
 
   const paginateOptions = {
     ...(offset && { offset }),
     ...(limit && { limit }),
     ...(sort && { sort }),
   };
-
   try {
     const streams = await Stream.paginate(JSON.parse(query), paginateOptions);
     res.status(200).json(streams);
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    next(error);
   }
 };
 
-export const createStream = async (req, res) => {
+export const createStream = async (req, res, next) => {
   const stream = req.body;
   const newStream = new Stream(stream);
 
@@ -26,6 +26,16 @@ export const createStream = async (req, res) => {
 
     res.status(200).json(newStream);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    next(error);
+  }
+};
+
+export const getStream = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const stream = await Stream.findById(id);
+    res.status(200).json(stream);
+  } catch (err) {
+    return next(new ErrorResponse("Stream not found", 404));
   }
 };
--- server/routes/StreamsRoute.js
+++ server/routes/StreamsRoute.js
@@ -1,10 +1,18 @@
 import express from "express";
 
-import { getStreams, createStream } from "../controller/StreamsController.js";
+import {
+  getStreams,
+  createStream,
+  getStream,
+} from "../controller/StreamsController.js";
+
+import { authorize } from "../middleware/AuthMiddleware.js";
+import { ADMIN, SUPERADMIN } from "../constants/userRoles.js";
 
 const router = express.Router();
 
 router.get("/", getStreams);
-router.post("/", createStream);
+router.get("/:id", getStream);
+router.route("/").post(authorize([SUPERADMIN, ADMIN]), createStream);
 
 export default router;
