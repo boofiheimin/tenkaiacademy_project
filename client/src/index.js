ac9ea245e6daecf72e67efb67ab7fabb7f063aee
Mheekwai Na Bangkok <boofiheimin@gmail.com>
2021-07-25 00:11:46 +0700

    fix no used var

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import momentDurationFormatSetup from "moment-duration-format";
 import moment from "moment";
