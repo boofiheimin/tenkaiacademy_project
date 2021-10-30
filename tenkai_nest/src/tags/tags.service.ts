780aa385b91a0d526746abf2b6b9a8792c197eea
boofi <boofiheimin@gmail.com>
2021-10-30 19:45:36 +0700

    :sparkles: Implement TagService

--- tenkai_nest/src/tags/tags.service.ts
+++ tenkai_nest/src/tags/tags.service.ts
@@ -7,6 +7,44 @@ import { Tag } from "./tag.schema";
 @Injectable()
 export class TagsService extends BaseService<Tag> {
     constructor(@InjectModel(Tag.name) private tagModel: Model<Tag>) {
-        super(tagModel, new Logger(TagsService.name));
+        super(tagModel, new Logger(TagsService.name), {
+            tagId: 1,
+            catId: 1,
+            tagNameEN: 1,
+            tagNameJP: 1,
+            isClip: 1,
+        });
+    }
+
+    private async findLatestTag(): Promise<Tag> {
+        const [latestTag] = await this.tagModel.find().sort({ tagId: -1 }).limit(1);
+        return latestTag;
+    }
+
+    public async createTag(data: Partial<Tag>): Promise<Tag> {
+        const latestTag = await this.findLatestTag();
+        let index;
+        if (!latestTag) {
+            index = 1;
+        } else {
+            index = latestTag.tagId + 1;
+        }
+        return this.create({ ...data, tagId: index });
+    }
+
+    public async updateTag(id: string, data: Partial<Tag>): Promise<Tag> {
+        const tag = await this.update(id, data);
+
+        //TODO:: Add Video/Clip Cascade update
+
+        return tag;
+    }
+
+    public async deleteTag(id: string): Promise<Tag> {
+        const tag = await this.delete(id);
+
+        //TODO:: Add Video/Clip Cascade update
+
+        return tag;
     }
 }
