46f75693bc59ece758c89853ce32e899ff9496b1
Buffy <boofiheimin@gmail.com>
2021-11-04 16:33:53 +0700

    🧪 Setup Test env for ClipsService

---
+++ tenkai_nest/src/clips/__mocks__/clips.repository.ts
@@ -0,0 +1,1 @@
+export const ClipsRepository = jest.fn().mockReturnValue(() => {});
--- tenkai_nest/src/clips/test/clips.service.spec.ts
+++ tenkai_nest/src/clips/test/clips.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { YoutubeService } from 'src/base/youtube.service';
+import { TagsService } from 'src/tags/tags.service';
+import { VideosService } from 'src/videos/videos.service';
+import { ClipsRepository } from '../clips.repository';
 import { ClipsService } from '../clips.service';
+
+jest.mock('../clips.repository');
+jest.mock('src/videos/videos.service');
+jest.mock('src/base/youtube.service');
+jest.mock('src/tags/tags.service');
 
 describe('ClipsService', () => {
     let service: ClipsService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [ClipsService],
+            providers: [ClipsService, ClipsRepository, VideosService, YoutubeService, TagsService],
         }).compile();
 
         service = module.get<ClipsService>(ClipsService);
