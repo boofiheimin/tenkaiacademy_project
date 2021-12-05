be764a39f4ce6d63c276b7b4e7f6bfe5875f1f9d
buffy <boofiheimin@gmail.com>
2021-12-05 15:44:20 +0700

    ✏️ missing changes from last commit

--- tenkai_next/pages/archive/videos/index.tsx
+++ tenkai_next/pages/archive/videos/index.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import { VideoLists } from '../../../components/VideoLists';
+import { VideosResponse } from '../../../interfaces/video.interface';
+import { fetchVideos } from '../../../lib/api';
 
 const Videos = () => {
-    return <VideoLists />;
+    const [data, setData] = useState<VideosResponse>({
+        docs: [],
+        totalCount: 0,
+    });
+    useEffect(() => {
+        async function fetch() {
+            const response = await fetchVideos();
+            setData(response);
+        }
+        fetch();
+    }, []);
+    return <VideoLists data={data} />;
 };
 export default Videos;
